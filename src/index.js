d0732550914ef0ec0e625d6a3a0db23413056c89
Denis Koltsov <denis_koltsov@yahoo.com>
2016-09-02 00:17:06 -0400

    deployment try again

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App.js';
 import Favorites from './Favorites.js';
 import Main from './Main.js';
 import MyProfile from './MyProfile.js';
-import About from './About.js';
 import './css/index.css';
 
 import {Router, Route, browserHistory} from 'react-router';
@@ -16,7 +15,6 @@ ReactDOM.render(
       <Route path="/myprofile" component={MyProfile}>
         <Route path="/favorites" component={Favorites}></Route>
       </Route>
-      <Route path="/about" component={About}></Route>
     </Route>
   </Router>,
   document.getElementById('root')
